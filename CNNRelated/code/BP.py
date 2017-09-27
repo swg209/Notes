cd5f5c6334b657c623e21326ae80fa4f68e4e6a9
junyanwu <wujunyan@csu.edu.cn>
2017-09-27 16:42:30 +0800

    del

--- CNNRelated/code/BP.py
+++
@@ -1,9 +0,0 @@
-# -*- coding:utf-8 -*-
-class Neural:
-    def __init__(self, activation):
-        self.activation = activation
-
-
-    def Activation(self, func_name):
-        if func_name == 'sigmod':
-            pass
